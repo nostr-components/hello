87b0dda8fec9f56b131959e06fd5dbe051fa7e13
Melvin Carvalho <melvincarvalho@gmail.com>
2023-05-21 18:09:27 +0200

    sidebar with links

--- js/app.js
+++ js/app.js
@@ -3,10 +3,14 @@ import '../js/nostr-ui.js'
 import Sidebar from '../components/Sidebar.js'
 import Navbar from '../components/Navbar.js'
 
+const links = [
+  { '@id': '#', label: 'Sidebar' }
+]
+
 render(html`<${Navbar} />
 
-<${Sidebar} />
+<${Sidebar} links="${links}" />
 
-<div style="padding-left: 220px; padding-top: 20px ">${di.data.name}</div>
+<div style="padding-left: 220px; padding-top: 20px"> ${di.data.name}</div>
 
 `, document.body)
