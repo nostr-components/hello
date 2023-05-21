f9db7ed865b84b7db388ae52c792e8dfc8b943b6
Melvin Carvalho <melvincarvalho@gmail.com>
2023-05-21 15:47:30 +0200

    rename Sidebar

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 import { html, render } from '../js/standalone.module.js'
 import '../js/nostr-ui.js'
-import Sidebar from '../js/nostr-components-sidebar.js'
+import Sidebar from '../components/Sidebar.js'
 import Navbar from '../components/Navbar.js'
 
 render(html`<${Navbar} />
-<${Sidebar} />`, document.body)
+
+<${Sidebar} />
+
+Hello Nostr!
+
+`, document.body)
