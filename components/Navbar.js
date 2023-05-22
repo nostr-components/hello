b155aabab3d1566ba809aa16b302f793e25bb953
Melvin Carvalho <melvincarvalho@gmail.com>
2023-05-22 14:04:19 +0200

    add navbar login

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,46 +1,67 @@
-import { html } from '../js/standalone.module.js'
+import { html, Component } from 'https://unpkg.com/htm/preact/standalone.module.js'
 
-const navbarStyle = {
-  overflow: 'hidden',
-  backgroundColor: '#333',
-  paddingLeft: '200px' // Added this line
-}
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      links: [],
+      isAuthenticated: false,
+      user: { name: 'John Doe' }
+    };
+  }
 
-const navbarLinkStyle = {
-  float: 'left',
-  display: 'block',
-  color: '#f2f2f2',
-  textAlign: 'center',
-  padding: '14px 16px',
-  textDecoration: 'none'
-}
+  handleLogin = async () => {
+    const cacheUri = 'https://nostr.social';
+    console.log('handleLogin');
+    var pubkey = await window.nostr.getPublicKey();
+    fetch(`${cacheUri}/.well-known/nostr/pubkey/${pubkey}/index.json`).then(response => response.json()).then(data => {
+      console.log(data);
+      this.setState({ user: data });
+    });
 
-const navbarLinkHoverStyle = {
-  backgroundColor: '#ddd',
-  color: 'black'
-}
 
-const dropdownStyle = {
-  float: 'left',
-  overflow: 'hidden'
-}
 
-const Navbar = ({ links = [] }) => {
-  if (links.length === 0) {
-    links = [
+
+    this.setState({ user: { name: pubkey.substring(0, 8) } });
+
+    this.setState({ isAuthenticated: !this.state.isAuthenticated });
+
+  };
+
+  render() {
+    const links = [
       { '@id': '#home', label: 'Home' },
       { '@id': '#about', label: 'About' },
       { '@id': '#contact', label: 'Nostr' }
     ]
-  }
 
-  return html`
-    <div style=${navbarStyle}>
-    ${links.map(link => html`
-    <a href=${link['@id']} style=${navbarLinkStyle} onMouseOver=${navbarLinkHoverStyle}>${link.label}</a>
-  `)}
-    </div>
-  `
+    const loginButtonText = this.state.isAuthenticated ? `Welcome, ${this.state.user.name}` : 'Login';
+
+    return html`
+      <nav style="display: flex; justify-content: space-between; padding: 10px 50px; background-color: #f8f9fa; border-bottom: 1px solid #e5e5e5; box-sizing: border-box;">
+        <div>
+          ${links.map(link => html`
+          
+            <a href=${link['@id']} 
+              style="color: #6c757d; text-decoration: none; margin-left: 140px; padding: 0 15px;"
+              onmouseover=${e => e.currentTarget.style.color = '#343a40'}
+              onmouseout=${e => e.currentTarget.style.color = '#6c757d'}
+            >
+              ${link.label}
+            </a>
+          `)}
+        </div>
+        <button 
+          style="color: #6c757d; text-decoration: none; padding: 0 15px; border: none; background: none;"
+          onmouseover=${e => { e.currentTarget.style.color = '#343a40'; e.currentTarget.style.cursor = 'pointer'; }}
+          onmouseout=${e => { e.currentTarget.style.color = '#6c757d'; e.currentTarget.style.cursor = 'default'; }}
+          onclick=${this.handleLogin}
+        >
+          ${loginButtonText}
+        </button>
+      </nav>
+    `;
+  }
 }
 
 export default Navbar
